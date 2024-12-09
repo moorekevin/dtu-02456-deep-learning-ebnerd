c942346a10d2957267e0f110f0134d9a5a295f0e
Kevin Moore <kmmiras@gmail.com>
2024-12-09 15:21:45 +0100

    Update nrms_torch-optimized.py

--- our_implementation/nrms_torch-optimized.py
+++ our_implementation/nrms_torch-optimized.py
@@ -21,7 +21,8 @@ import sys
 import os
 
 # Add the parent directory to sys.path
-sys.path.append(os.path.abspath(".."))
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(root_dir)
 print(sys.path)
 # In[18]:
 
